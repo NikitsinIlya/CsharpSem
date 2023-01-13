0b211ddc8d9af72b3fadca76f8b02b09269230b3
NikitsinIlya <JamsN87@gmail.com>
2023-01-13 11:42:57 +0300

    Zadacha3

--- Zadacha3/Program.cs
+++ Zadacha3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Clear();
+Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// && - и(логическое умножение)
+// || - или(логическое сложение)
+// True - 1
+// False - 0
+//       1    +   0   = 1(True)
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1)
+    Console.WriteLine("Понедельник");
+else if (n == 2)
+    Console.WriteLine("Вторник");
+else if (n == 3)
+    Console.WriteLine("Среда");
+else if (n == 4)
+    Console.WriteLine("Четверг");
+else if (n == 5)
+    Console.WriteLine("Пятница");
+else if (n == 6)
+    Console.WriteLine("Суббота");
+else
+    Console.WriteLine("Воскресенье");
