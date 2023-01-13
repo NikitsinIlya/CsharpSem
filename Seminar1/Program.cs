e2ebf448c3c735d314dc921033bbce2bceb042b2
NikitsinIlya <JamsN87@gmail.com>
2023-01-13 11:00:31 +0300

    Add Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Internal;
+using System;
+/*
+Console.Clear();
+Console.Write();
+Console.WriteLine($"{a}, {b}"); // - интерполяция вывод самих данных
+
+int n = Convert.ToInt32(Console.ReadLine()); // перевод чисел из строки в цифры
+int n = int.Parse(Console.ReadLine());  // перевод чисел из строки в цифры
+
+
+Console.Write("Введите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+    Console.WriteLine(m);
+else if (n < m)
+    Console.WriteLine(m);
+else 
+    Console.WriteLine("=");
+    */
