27e785f5bb6da47c74d5cc05a13bb166d27589f2
NikitsinIlya <JamsN87@gmail.com>
2023-01-13 11:55:09 +0300

    Zadacha 5

---
+++ Zadacha5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n * (-1);
+
+while (m <= n )
+{
+    Console.Write($"{m} ");
+    m = m + 1; //m++;
+
+}
